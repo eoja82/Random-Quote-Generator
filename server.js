b316d0ff7234a69d178488df25ad91679627c040
Erik Oja <erik_oja@yahoo.com>
2020-03-16 16:21:50 -0500

    remove comment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const request     = require("request");
 require("dotenv").config();
 const app = express();
 
@@ -9,16 +8,6 @@ app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   })
-
-/* app.route('/newquote')
-  .get(function (req, res) {
-    let url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
-    request (url, {json: true}, function (err, resp, body) {
-      if (err) { console.log(err); }
-      else if (!body) { res.json({"error": "something went wrong"}); }
-      else { res.json(body); }
-    });
-  }) */
 
 //404 Not Found Middleware
 app.use(function(req, res, next) {
